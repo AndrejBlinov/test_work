3bc34877563c7a8e8d8bcba27c2c4da5e91fb86c
андрей <andrej.blinov.1991@mail.ru>
2021-03-08 08:53:15 +0300

    with search

--- index.php
+++ index.php
@@ -1,9 +1,211 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Тестовая работа");
-?>
+?><section>
+<div class="col-md-12 col-sm-12 col-xs-12">
+	<div>
+		<ul class="nav nav-tabs">
+			<li class="nav-item"> <a class="nav-link active" data-toggle="tab" href="#quests">Задачи</a> </li>
+			<li class="nav-item"> <a class="nav-link" data-toggle="tab" href="#worker">Исполнители</a> </li>
+		</ul>
+	</div>
+	<div class="tab-content">
+		 <!-- Tab Pane -->
+		<div class="tab-pane active" id="quests">
+<?$APPLICATION->IncludeComponent(
+	"bitrix:search.title",
+	"visual",
+	Array(
+		"CATEGORY_0" => array("iblock_quest"),
+		"CATEGORY_0_TITLE" => "",
+		"CATEGORY_0_iblock_quest" => array("3"),
+		"CHECK_DATES" => "N",
+		"CONTAINER_ID" => "title-search",
+		"INPUT_ID" => "title-search-input",
+		"NUM_CATEGORIES" => "1",
+		"ORDER" => "rank",
+		"PAGE" => "#SITE_DIR#search/index.php",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PRICE_CODE" => array(),
+		"PRICE_VAT_INCLUDE" => "Y",
+		"SHOW_INPUT" => "Y",
+		"SHOW_OTHERS" => "N",
+		"SHOW_PREVIEW" => "Y",
+		"TOP_COUNT" => "5",
+		"USE_LANGUAGE_GUESS" => "Y"
+	)
+);?>
 
+			 <?$APPLICATION->IncludeComponent(
+	"bitrix:news.list",
+	"test_work",
+	Array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"COMPONENT_TEMPLATE" => "test_work",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => array(0=>"",1=>"",),
+		"FILTER_NAME" => "",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => "3",
+		"IBLOCK_TYPE" => "-",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+		"INCLUDE_SUBSECTIONS" => "N",
+		"MEDIA_PROPERTY" => "",
+		"MESSAGE_404" => "",
+		"NEWS_COUNT" => "20",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Новости",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PROPERTY_CODE" => array(0=>"",1=>"STATUS",2=>"WORKER",),
+		"SEARCH_PAGE" => "/search/",
+		"SET_BROWSER_TITLE" => "N",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "N",
+		"SET_META_KEYWORDS" => "N",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "N",
+		"SHOW_404" => "N",
+		"SLIDER_PROPERTY" => "",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC",
+		"STRICT_SECTION_CHECK" => "N",
+		"TEMPLATE_THEME" => "blue",
+		"USE_RATING" => "N",
+		"USE_SHARE" => "N"
+	)
+);?>
+			<div class="col-md-4 col-md-offset-4">
+ <button type="button" class="btn btn-success" data-toggle="modal" data-target="#questAdd">Добавить</button>
+			</div>
+		</div>
+		<div class="tab-pane" id="worker">
+<?$APPLICATION->IncludeComponent(
+	"bitrix:search.title", 
+	"visual", 
+	array(
+		"CATEGORY_0" => array(
+			0 => "iblock_workers",
+		),
+		"CATEGORY_0_TITLE" => "",
+		"CATEGORY_0_iblock_quest" => array(
+			0 => "4",
+		),
+		"CHECK_DATES" => "N",
+		"CONTAINER_ID" => "title-search-workers",
+		"INPUT_ID" => "title-search-input-workers",
+		"NUM_CATEGORIES" => "1",
+		"ORDER" => "rank",
+		"PAGE" => "#SITE_DIR#search/index.php",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PRICE_CODE" => array(
+		),
+		"PRICE_VAT_INCLUDE" => "N",
+		"SHOW_INPUT" => "Y",
+		"SHOW_OTHERS" => "N",
+		"SHOW_PREVIEW" => "Y",
+		"TOP_COUNT" => "5",
+		"USE_LANGUAGE_GUESS" => "Y",
+		"COMPONENT_TEMPLATE" => "visual",
+		"PREVIEW_WIDTH" => "75",
+		"PREVIEW_HEIGHT" => "75",
+		"CATEGORY_0_iblock_workers" => array(
+			0 => "4",
+		)
+	),
+	false
+);?>
 
-
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+			 <?$APPLICATION->IncludeComponent(
+	"bitrix:news.list",
+	"worker_list",
+	Array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"COMPONENT_TEMPLATE" => "worker_list",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => array(0=>"",1=>"",),
+		"FILTER_NAME" => "",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => "4",
+		"IBLOCK_TYPE" => "workers",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+		"INCLUDE_SUBSECTIONS" => "N",
+		"MEDIA_PROPERTY" => "",
+		"MESSAGE_404" => "",
+		"NEWS_COUNT" => "20",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Новости",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PROPERTY_CODE" => array(0=>"",1=>"POST",),
+		"SEARCH_PAGE" => "/search/",
+		"SET_BROWSER_TITLE" => "N",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "N",
+		"SET_META_KEYWORDS" => "N",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "N",
+		"SHOW_404" => "N",
+		"SLIDER_PROPERTY" => "",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC",
+		"STRICT_SECTION_CHECK" => "N",
+		"TEMPLATE_THEME" => "blue",
+		"USE_RATING" => "N",
+		"USE_SHARE" => "N"
+	)
+);?> <button type="button" class="btn btn-success" data-toggle="modal" data-target="#workerAdd">Добавить</button>
+		</div>
+	</div>
+</div>
+ </section><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
